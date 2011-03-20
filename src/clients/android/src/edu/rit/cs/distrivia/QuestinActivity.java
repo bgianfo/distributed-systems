2183b798b49d43ef9d33b91a60eed9d8e7565bda
Brian Gianforcaro <b.gianfo@gmail.com>
2011-03-20 15:15:56 -0400

    Initialize an Eclipse Project for the android client.

---
+++ src/clients/android/src/edu/rit/cs/distrivia/QuestinActivity.java
@@ -0,0 +1,13 @@
+package edu.rit.cs.distrivia;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class QuestinActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
