ab3244e072556d0a747e76d4d7c4b7af92d241e6
Brian Gianforcaro <b.gianfo@gmail.com>
2011-03-22 01:48:24 -0400

    Implement registration using Riak, add doc..

--- src/server/distrivia.py
+++ src/server/distrivia.py
@@ -1,12 +1,65 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    Flaskr
+    ~~~~~~
+
+    A microblog example application written as Flask tutorial with
+    Flask and sqlite3.
+
+    :copyright: (c) 2011 by Brian Gianforcaro, Steven Glazer, Sam Milton.
+    :license: BSD, see LICENSE for more details.
+"""
 
 from flask import Flask
 from flask import json
 from flask import request
+from flask import session
+from flask import g
 
-app = Flask("Distrivia")
+import riak
+
+app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['SECRET_KEY'] = 'secret'
+
+API_ERROR   = "err"
+API_SUCCESS = "suc"
+
+#
+# Before and after request handlers
+#
+
+@app.before_request
+def before_request():
+    """ Connect to Riak before each request """
+    client = riak.RiakClient()
+    g.db = client
+
+@app.after_request
+def after_request(response):
+    """ Closes Riak connection after each request """
+    g.db = None
+    return response
+
+#
+# Application routes
+#
+
+@app.route('/register')
+def register():
+    """ Register a new user """
+
+    user = request.form['uname']
+    users_bucket = g.db.bucket( "users" )
+
+    if users_bucket.get( user ).exists():
+        return API_ERROR
+    else:
+        newUser = users_bucket.new( user, data={} )
+        newUser.store()
+        return API_SUCCESS
+
 
 @app.route('/game/new')
 def new_game():
@@ -19,7 +72,7 @@ def new_game():
 def join_game():
     """GET /game/join
 
-    Join a currently open a new game if available,
+    Join a currently open game if available,
     otherwise create a new one"""
     return "NOT IMPLEMEMTED"
 
@@ -28,16 +81,16 @@ def next_question(id):
     """GET /game/<id>/next
 
     Get the next question for this game ID"""
-    return "NOT IMPLEMENTED" 
+    return "NOT IMPLEMENTED"
 
 @app.route('/game/<id>/leaderboard')
 def get_leaderboard(id):
     """GET /game/<id>/leaderboard
 
     Get the leaderboard for this game ID"""
-    return "NOT IMPLEMENTED" 
+    return "NOT IMPLEMENTED"
 
 
-# Run the webserver 
+# Run the webserver
 if __name__ == '__main__':
-    app.run()
+    app.run(port=80)
--- src/setup-all.sh
+++ src/setup-all.sh
@@ -4,11 +4,15 @@ INSTANCE_IPS=`ec2-describe-instances | grep INSTANCE | cut -f4`
 INTERNAL_IPS=`ec2-describe-instances | grep INSTANCE | cut -f18`
 KEY=~/.ec2/id_rsa-brian-keypair
 
+# Open Riak port numbers
 ec2-authorize default -p 8098
 ec2-authorize default -p 8099
+
+# Open app server port numbers
 ec2-authorize default -p 80
 ec2-authorize default -p 22
 
+# Provision and setup all instances
 for IP in $INSTANCE_IPS
 do
     # Copy over our provisioning script
@@ -19,12 +23,14 @@ do
     ssh -t -i $KEY ec2-user@$IP ./setup-node.sh
 done
 
+# Start all the riak instances
 for IP in $INSTANCE_IPS
 do
     ssh -t -i $KEY ec2-user@$IP riak stop
     ssh -t -i $KEY ec2-user@$IP riak start
 done
 
+# Link up all the Riak servers
 for IP in $INSTANCE_IPS
 do
     ssh -t -i $KEY ec2-user@$IP ./join-node.sh  $INTERNAL_IPS
