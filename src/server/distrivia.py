d55136fb6073ec7ce16ea14deb4b96c96ffb72bb
Brian Gianforcaro <b.gianfo@gmail.com>
2011-03-24 02:51:19 -0400

    Add login, leaderboard, and next question on server.
    - Flesh out initial implementations for the following methods:
    
      * Login: authenticate, create and return an authorization token
    
      * Leader Board: Display a score board for all users in the current game.
    
      * Next Question: Retreive the next question for the current user
    
    - Add global isAuthed() method which can magically tell if any API call
      is authenticated. It's based on the authentication token being passed in.
    
    - Clean up code a little bit, fixing stale comments, etc.

--- src/server/distrivia.py
+++ src/server/distrivia.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-    Flaskr
-    ~~~~~~
-
-    A microblog example application written as Flask tutorial with
-    Flask and sqlite3.
+     Distrivia
+    ~~~~~~~~~~~
+    A webservice backend which implements a RESTFUL trivia game API.
 
     :copyright: (c) 2011 by Brian Gianforcaro, Steven Glazer, Sam Milton.
-    :license: BSD, see LICENSE for more details.
 """
 
 from flask import Flask
@@ -18,6 +15,7 @@ from flask import session
 from flask import g
 
 import riak
+import uuid
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -25,19 +23,31 @@ app.config['SECRET_KEY'] = 'secret'
 
 API_ERROR   = "err"
 API_SUCCESS = "suc"
+QUESTION_LIMIT = 20
+
+
+#
+# Utility functions
+#
+
+def isAuthed():
+    token = request.form['authToken']
+    loggedIn = g.db.bucket("users").get("logged_in")
+    users = loggedIn.get_data()["users"]
+    return token in users
 
 #
 # Before and after request handlers
 #
 
 @app.before_request
-def before_request():
+def beforeRequest():
     """ Connect to Riak before each request """
     client = riak.RiakClient()
     g.db = client
 
 @app.after_request
-def after_request(response):
+def afterRequest(response):
     """ Closes Riak connection after each request """
     g.db = None
     return response
@@ -46,50 +56,121 @@ def after_request(response):
 # Application routes
 #
 
-@app.route('/register')
-def register():
+@app.route('/register/<user>' method=["PUT"])
+def register(user):
     """ Register a new user """
 
-    user = request.form['uname']
     users_bucket = g.db.bucket( "users" )
 
+    # The user already exists, so they can't register this name
     if users_bucket.get( user ).exists():
         return API_ERROR
+
+    # Awesome, let's register this bad boy
     else:
-        newUser = users_bucket.new( user, data={} )
+        newUser = users_bucket.new( user, data= { "uuid" : "" } )
         newUser.store()
         return API_SUCCESS
 
+@app.route('/login/<userName>', method=["PUT"])
+deff login( userName ):
+    """ Attempt to authenticate a user, return a auth token on success """
+
+    users = g.db.bucket( "users" )
+    user = users.get( userName )
+
+    if user.exists():
+        # Create a new auth token for this user
+        # UUID's are pretty much guaranteed to be unique
+        uniqueId = str(uuid.uuid1())
+        user.set_data( { "uuid" : uniqueId } )
+        user.store()
+
+        loggedIn = users.get("logged_in")
+        data = loggedIn.get_data()
+        data["users"].append( uniqueId )
+        loggedIn.set_data(data)
+        loggedIn.store()
+
+        return uniqueId
+    else:
+        return API_ERROR
 
 @app.route('/game/new')
-def new_game():
+def newGame():
     """GET /game/new
 
     Create a new game"""
     return "NOT IMPLEMEMTED"
 
 @app.route('/game/join')
-def join_game():
+def joinGame():
     """GET /game/join
 
     Join a currently open game if available,
     otherwise create a new one"""
     return "NOT IMPLEMEMTED"
 
-@app.route('/game/<id>/next')
-def next_question(id):
+@app.route('/game/<id>/next/<prevId>')
+def nextQuestion(id,prevId):
     """GET /game/<id>/next
 
     Get the next question for this game ID"""
-    return "NOT IMPLEMENTED"
+
+    # Failure on authentication error
+    if not isAuthed():
+        return API_ERROR
+
+    gamesBucket = g.db.bucket( "games" )
+    game = gamesBucket.get( id )
+
+    # Failure on bogus game ID
+    if game.empty():
+        return API_ERROR
+    else:
+        # Get the list of questions for this game
+        gameData = game.get_data();
+        questions = gameData["questions"]
+        qIndex = questions.index( prevId )
+
+        # Failure on this game being over
+        if qIndex == QUESTION_LIMIT:
+            return API_ERROR:
+
+        # Fetch and construct the next question object
+        nextQuestion = questions[qIndex+1]
+        questionBucket = g.db.bucket( "questions" )
+        question = questionBucket.get( nextQuestion );
+        question["id"] = nextQuestion
+        return str(question)
 
 @app.route('/game/<id>/leaderboard')
-def get_leaderboard(id):
-    """GET /game/<id>/leaderboard
+def getLeaderBoard(id):
+    """
+    Get the leaderboard for this game ID
 
-    Get the leaderboard for this game ID"""
-    return "NOT IMPLEMENTED"
+    Note: The leader board should be a simple JSON object.
+    {
+        <username> : <points>
+        "user1"    : 1000
+        "user2"    : 3000
+        "user2"    : 5000
+    }
+    """
+    # Failure on authentication error
+    if not isAuthed():
+        return API_ERROR
 
+    gamesBucket = g.db.bucket( "games" )
+    game = gamesBucket.get( id )
+
+    # Failure on bogus game ID
+    if game.empty():
+        return API_ERROR
+    else:
+        # Get the list of questions for this game
+        gameData = game.get_data();
+        return str(gameData["leaderboard"])
 
 # Run the webserver
 if __name__ == '__main__':
