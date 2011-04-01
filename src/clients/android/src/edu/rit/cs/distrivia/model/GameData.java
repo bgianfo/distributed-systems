1a71f83d761fc7e9fb4cae55c03693a5e2143119
Brian Gianforcaro <b.gianfo@gmail.com>
2011-04-01 16:19:12 -0400

    Game data and Question Activity updates

--- src/clients/android/src/edu/rit/cs/distrivia/model/GameData.java
+++ src/clients/android/src/edu/rit/cs/distrivia/model/GameData.java
@@ -1,26 +1,63 @@
 package edu.rit.cs.distrivia.model;
 
+import java.io.Serializable;
+
 /**
  *
  */
-public class GameData {
+public class GameData implements Serializable {
 
-    private String authToken;
-    private String userName;
+    private static final long serialVersionUID = -4175459527542039101L;
+    private final String authToken;
+    private final String userName;
     private String gameID;
 
-    private GameData() {
-
+    /**
+     * Constuct a semi-immutable GameData object
+     * 
+     * @param authToken
+     *            Auth token logged in with
+     * @param userName
+     *            Username of the current session
+     */
+    public GameData(String authToken, String userName) {
+        this.authToken = authToken;
+        this.userName = userName;
     }
 
+    /**
+     * Setter for game Id, can change in between different rounds/games.
+     * 
+     * @param id
+     *            The current Game Id String
+     */
+    public void setGameId(String id) {
+        this.gameID = id;
+    }
+
+    /**
+     * Getter for game ID
+     * 
+     * @return The current game ID String
+     */
     public String getGameID() {
         return gameID;
     }
 
+    /**
+     * Getter for user name
+     * 
+     * @return The logged in user name.
+     */
     public String getUserName() {
         return userName;
     }
 
+    /**
+     * Getter for authentication token
+     * 
+     * @return The current session authentication token
+     */
     public String getAuthToken() {
         return authToken;
     }
