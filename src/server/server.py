87187546dac91c2108a848f9f1b0ace8c47d09ae
Brian Gianforcaro <b.gianfo@gmail.com>
2011-04-01 10:19:41 -0400

    Add new high performance server backend, add SSL mode!

---
+++ src/server/server.py
@@ -0,0 +1,22 @@
+from tornado.wsgi import WSGIContainer
+from tornado.httpserver import HTTPServer
+from tornado.ioloop import IOLoop
+import tornado.options
+from distrivia import app
+
+
+
+SSL_PORT = 443
+
+ssl_files = {
+ "certfile" : "ssl-cert/distrivia.lame.ws.crt",
+ "keyfile"  : "ssl-cert/distrivia.lame.ws.key",
+ "ca_certs" : "ssl-cert/distrivia.lame.ws.ca-bundle"
+}
+
+tornado.options.parse_command_line()
+
+wsgiApp = WSGIContainer(app)
+http_server = HTTPServer(wsgiApp, ssl_options=ssl_files)
+http_server.listen(443)
+IOLoop.instance().start()
