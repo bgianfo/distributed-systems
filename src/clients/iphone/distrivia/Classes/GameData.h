edb55f90ade2475c08d21a4bb692e85a2ebac15c
Brian Gianforcaro <b.gianfo@gmail.com>
2011-04-20 13:51:14 -0400

    Add simple stupid game data object to be passed around.

---
+++ src/clients/iphone/distrivia/Classes/GameData.h
@@ -0,0 +1,31 @@
+//
+//  GameData.h
+//  distrivia
+//
+//  Created by Brian Gianforcaro on 4/20/11.
+//  Copyright 2011 Rochester Institute of Technology. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface GameData : NSObject {
+ NSString * status;
+ NSString * token;
+ NSString * username;
+ int score;
+}
+- (BOOL) hasStarted;
+
+- (BOOL) isDone;
+
+- (int) getScore;
+
+- (NSString*) getUser;
+
+- (void) setScore:(int)score;
+
+- (void) setStatus:(NSString*) status;
+
+- (void) setUser:(NSString*) user;
+@end
---
+++ src/clients/iphone/distrivia/Classes/GameData.m
@@ -0,0 +1,53 @@
+//
+//  GameData.m
+//  distrivia
+//
+//  Created by Brian Gianforcaro on 4/20/11.
+//  Copyright 2011 Rochester Institute of Technology. All rights reserved.
+//
+
+#import "GameData.h"
+
+
+@implementation GameData
+
+- (id) init {
+    
+    status = nil;
+    username = nil;
+    score = 0;
+    
+    
+    return self;
+}
+
+- (BOOL) hasStarted {
+    return [status isEqualToString:@"starting"];
+}
+
+- (BOOL) isDone {
+    return [status isEqual:@"done"];
+}
+
+- (int) getScore {
+    return score;
+}
+
+- (NSString*) getUser {
+    return username;
+}
+
+- (void) setScore:(int) newScore {
+    score = newScore;
+}
+
+- (void) setStatus:(NSString*) newStatus {
+    [newStatus retain];
+    status = newStatus;
+}
+
+- (void) setUser:(NSString*) user {
+    [user retain];
+    username = user;
+}
+@end
