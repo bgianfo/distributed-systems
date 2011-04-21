08b1fa63a32e3562e7bde76c424bcaf45b72af17
Sticky Glazer <sfg6126@rit.edu>
2011-04-21 00:40:06 -0400

    Fancied up join screen for iphone and added stuff to get keyboards to drop and added another activity indicator

--- src/clients/iphone/distrivia/Classes/JoinViewController.m
+++ src/clients/iphone/distrivia/Classes/JoinViewController.m
@@ -12,9 +12,13 @@
 @implementation JoinViewController
 
 @synthesize delegate;
-@synthesize pubButton;
-@synthesize priButton;
-@synthesize leadButton;
+@synthesize pubBut;
+@synthesize priJoinBut;
+@synthesize priCreateBut;
+@synthesize leadBut;
+@synthesize nameField;
+@synthesize passField;
+@synthesize activeIndicate;
 
 // The designated initializer.  Override if you create the controller programmatically and want to perform customization that is not appropriate for viewDidLoad.
 /*
@@ -33,16 +37,18 @@
     UIImage *buttonImageNormal = [UIImage imageNamed:@"whiteButton.png"];
     UIImage *stretchableButtonImageNormal = [buttonImageNormal stretchableImageWithLeftCapWidth:12 
                                                                                    topCapHeight:0];
-    [priButton setBackgroundImage:stretchableButtonImageNormal forState:UIControlStateNormal];
-    [pubButton setBackgroundImage:stretchableButtonImageNormal forState:UIControlStateNormal];
-    [leadButton setBackgroundImage:stretchableButtonImageNormal forState:UIControlStateNormal];
+    [priJoinBut setBackgroundImage:stretchableButtonImageNormal forState:UIControlStateNormal];
+    [priCreateBut setBackgroundImage:stretchableButtonImageNormal forState:UIControlStateNormal];
+    [pubBut setBackgroundImage:stretchableButtonImageNormal forState:UIControlStateNormal];
+    [leadBut setBackgroundImage:stretchableButtonImageNormal forState:UIControlStateNormal];
     
     UIImage *buttonImagePressed = [UIImage imageNamed:@"blueButton.png"];
     UIImage *stretchableButtonImagePressed = [buttonImagePressed stretchableImageWithLeftCapWidth:12 
                                                                                      topCapHeight:0];
-    [priButton setBackgroundImage:stretchableButtonImagePressed forState:UIControlStateHighlighted];
-    [pubButton setBackgroundImage:stretchableButtonImagePressed forState:UIControlStateHighlighted];
-    [leadButton setBackgroundImage:stretchableButtonImagePressed forState:UIControlStateHighlighted];
+    [priJoinBut setBackgroundImage:stretchableButtonImagePressed forState:UIControlStateHighlighted];
+    [priCreateBut setBackgroundImage:stretchableButtonImagePressed forState:UIControlStateHighlighted];
+    [pubBut setBackgroundImage:stretchableButtonImagePressed forState:UIControlStateHighlighted];
+    [leadBut setBackgroundImage:stretchableButtonImagePressed forState:UIControlStateHighlighted];
     [super viewDidLoad];
 }
 
@@ -64,6 +70,15 @@
 	[controller release];
 }
 
+- (IBAction) textFieldDoneEditing:(id)sender {
+    [sender resignFirstResponder];
+}
+
+- (IBAction) backgroundTap:(id)sender {
+    [nameField resignFirstResponder];
+    [passField resignFirstResponder];
+}
+
 - (void)didReceiveMemoryWarning {
     // Releases the view if it doesn't have a superview.
     [super didReceiveMemoryWarning];
@@ -74,17 +89,25 @@
 - (void)viewDidUnload {
     // Release any retained subviews of the main view.
 	self.delegate = nil;
-	self.pubButton = nil;
-    self.priButton = nil;
-    self.leadButton = nil;
+	self.pubBut = nil;
+    self.priJoinBut = nil;
+    self.priCreateBut = nil;
+    self.leadBut = nil;
+    self.nameField = nil;
+    self.passField = nil;
+    self.activeIndicate = nil;
 	[super viewDidUnload];
 }
 
 
 - (void)dealloc {
-	[pubButton release];
-    [priButton release];
-    [leadButton release];
+	[pubBut release];
+    [priJoinBut release];
+    [priCreateBut release];
+    [leadBut release];
+    [nameField release];
+    [passField release];
+    [activeIndicate release];
     [super dealloc];
 }
 
